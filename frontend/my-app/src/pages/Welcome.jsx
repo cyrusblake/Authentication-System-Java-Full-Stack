e8fcebeb7ef9bcb9d5ddbb0fe062e9bfd22fe8a9
Cyrus Blake <cyrusblake89@gmail.com>
2023-12-03 22:28:54 -0500

    more changes

--- frontend/my-app/src/pages/Welcome.jsx
+++ frontend/my-app/src/pages/Welcome.jsx
@@ -5,16 +5,16 @@ import '../styles/Welcome.css'
 function Welcome(){
     return(
         <>
-        <div className="contents">
-            <h1>Welcome</h1>
-            <div>
-                <Link to='/register'><button className="tb">Get started</button></Link>
-            </div>
-            <div>
-                <Link to='/login'><button className="wb">Log in</button></Link>
-            </div>
-            <p className="p">Terms & Conditions</p>
-            <p className="p">Privacy Policy</p>
+        <div className="wcontents">
+                <h1>Welcome</h1>
+                <div>
+                    <Link to='/register'><button className="tb">Get started</button></Link>
+                </div>
+                <div>
+                    <Link to='/login'><button className="wb">Log in</button></Link>
+                </div>
+                <p className="p">Terms & Conditions</p>
+                <p className="p">Privacy Policy</p>
         </div>
         </>
     )
