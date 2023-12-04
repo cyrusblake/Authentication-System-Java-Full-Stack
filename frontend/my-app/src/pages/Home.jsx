334ee2dc301372e1dae2a873374191305a3bda42
Cyrus Blake <cyrusblake89@gmail.com>
2023-12-04 06:01:04 -0500

    more changes

--- frontend/my-app/src/pages/Home.jsx
+++ frontend/my-app/src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import '../styles/Home.css'
 
 function Home(){
     return(
         <>
         <div className="contents">
-            <h1>Home</h1>
+            <h1>$0</h1>
+            <button className="hb">Request</button>
+            <button className="hb">Pay</button>
         </div>
         </>
     )
