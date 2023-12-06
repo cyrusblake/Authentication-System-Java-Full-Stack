ae5ba8056fce1b649d7fdfa016f3e34450259419
Cyrus Blake <cyrusblake89@gmail.com>
2023-12-05 19:00:48 -0500

    Update Home.jsx

--- frontend/my-app/src/pages/Home.jsx
+++ frontend/my-app/src/pages/Home.jsx
@@ -1,11 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import '../styles/Home.css'
 
 function Home(){
+
+
+    const[cash, setCash] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+    const id = '5'
+
+    useEffect(() => {
+        fetch(`http://localhost:8080/user/cash/${id}`)
+            .then(res => res.json())
+            .then(result => {
+                // Check if result is a number before updating state
+                if (typeof result === 'number') {
+                    setCash(result);
+                    setLoading(false); // Set loading to false when data is received
+                } else {
+                    console.error("API response is not a number:", result);
+                }
+            })
+            .catch(error => {
+                console.error("Error fetching data:", error);
+            });
+    }, [])
+
     return(
         <>
         <div className="contents">
-            <h1>$0</h1>
+            <div>
+                {loading ? (
+                    <p>Loading...</p>
+                ) : (
+                    <h1>${cash}</h1>
+                )}
+            </div>
             <button className="hb">Request</button>
             <button className="hb">Pay</button>
         </div>
